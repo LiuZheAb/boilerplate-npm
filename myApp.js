3cd23e1a1d7a906901aac154a9e00d5017f42141
liuzhe <liu748951@163.com>
2020-05-15 23:24:04 +0800

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,1 +1,9 @@
 console.log("Hello World");
+
+var findEditThenSave = function(personId, done) {
+    var foodToAdd = "hamburger";
+    Person.findById({ _id: personId }, function(err, data) {
+      data.favoriteFoods.push(foodToAdd);
+      
+    });
+  };
